b0f90fa282d1871449bbdc927d61dd5436a86e92
Mabooring <maboo2323@gmail.com>
2020-06-25 11:39:09 +0900

    AuthMiddleWareの挿入ミス修正

--- server/routes/roads.js
+++ server/routes/roads.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const Road = require("../model/road");
-const UserCtrl = require("../controller/user");
+const UserCtrl = require("../controllers/user");
 
-router.get("/secret", UserCtrl.authMiddleware, function (req, res) {
-  return res.json({ secret: true });
-});
+//DEBUG(postman用)
+// router.get("/secret", UserCtrl.authMiddleware, function (req, res) {
+//   return res.json({ secret: true });
+// });
 
 router.get("", function (req, res) {
   Road.find({}, function (err, foundRoads) {
@@ -13,9 +14,12 @@ router.get("", function (req, res) {
   });
 });
 
-router.get("/:roadId", function (req, res) {
+router.get("/:roadId", UserCtrl.authMiddleware, function (req, res) {
   const roadId = req.params.roadId;
-  Road.findById(roadId, UserCtrl.authMiddleware, function (err, foundRoad) {
+
+  //DEBUG
+  //Road.findById(roadId, UserCtrl.authMiddleware, function (err, foundRoad) {
+  Road.findById(roadId, function (err, foundRoad) {
     if (err) {
       return res
         .status(422)
