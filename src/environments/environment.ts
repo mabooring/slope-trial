5dab3976880197df82b0208268c47922dfbd6d4f
Mabooring <maboo2323@gmail.com>
2020-07-14 13:29:04 +0900

    ファイル削除

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
-
+export const AWS_ENV = {
+  region: 'ap-northeast-1',
+  s3: {
+    apiVersion: '2006-03-01',
+    Bucket: 's-trial-app',
+  },
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
